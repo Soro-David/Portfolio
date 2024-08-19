714264412baa5d6676f2fa20afa02c29042469f4
Soro-David <soroddavid63@gmail.com>
2024-08-19 23:48:51 +0000

    first build

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports={
+  publicPath:process.envNODE_ENV==='production' 
+  ?'https://github.com/Soro-David/Portfolio.git'
+  :'/'
+}
